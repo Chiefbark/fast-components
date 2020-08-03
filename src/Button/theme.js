1db7c77110c771bbd8358e394c12e94fbb982304
Jorge Chércoles Moreno <jchercoles@alumnos.imf.com>
2020-08-04 01:32:03 +0200

    added theme default values to Button component

---
+++ src/Button/theme.js
@@ -0,0 +1,31 @@
+const theme = {
+	root: {
+		default: {
+			display: 'flex',
+			flexDirection: 'row',
+			alignItems: 'center',
+			justifyContent: 'center',
+			borderRadius: '4px',
+			outline: 'none',
+			fontSize: '1.15rem',
+			borderStyle: 'solid',
+			padding: '16px 32px'
+		},
+		focus: {},
+		hover: {},
+		active: {},
+		disabled: {}
+	},
+	icon: {
+		default: {
+			width: '24px',
+			height: '24px'
+		},
+		focus: undefined,
+		hover: undefined,
+		active: undefined,
+		disabled: undefined
+	}
+}
+
+export default theme;
