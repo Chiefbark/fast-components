ff5223484fe6675cab81f26c886fe69a0114305c
Jorge Chércoles Moreno <jchercoles@alumnos.imf.com>
2020-08-04 01:35:46 +0200

    added return to function

--- src/utils/JSONToCSS/index.js
+++ src/utils/JSONToCSS/index.js
@@ -1,5 +1,5 @@
 function JSONToCSS(obj) {
-	Object.entries(obj)
+	return Object.entries(obj)
 		.map(([k, v]) =>
 			`${k.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`)}:${v}`
 		).join(';');
