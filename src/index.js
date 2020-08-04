4104cb1dd69f690d94c2b923b54e6a1d619b6e1d
Jorge Chércoles Moreno <jchercoles@alumnos.imf.com>
2020-08-04 16:05:25 +0200

    exported components

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import _Button from './Button';
+import _ButtonIcon from './ButtonIcon';
+import _Checkbox from './Checkbox';
+
+export const Button = _Button;
+export const ButtonIcon = _ButtonIcon;
+export const Checkbox = _Checkbox;
