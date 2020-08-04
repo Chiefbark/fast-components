077bb934b31994b4ed2e7c72e8573a846f73cb6e
Jorge Chércoles Moreno <jchercoles@alumnos.imf.com>
2020-08-04 03:29:43 +0200

    renamed files and improved functionality

--- src/utils/JSONToCSS/index.js
+++
@@ -1,8 +0,0 @@
-function JSONToCSS(obj) {
-	return Object.entries(obj)
-		.map(([k, v]) =>
-			`${k.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`)}:${v}`
-		).join(';');
-}
-
-export default JSONToCSS;
---
+++ src/utils/themeToCSS/index.js
@@ -0,0 +1,16 @@
+function themeToCSS(obj) {
+	return Object.keys(obj).map(x =>
+		Object.keys(obj[x]).map(y =>
+			y === 'default' ? `.${x}{${getStyles(obj[x][y])}}` : `.${x}${y}{${getStyles(obj[x][y])}}`
+		).join('')
+	).join('');
+}
+
+function getStyles(obj) {
+	return Object.entries(obj)
+		.map(([k, v]) =>
+			`${k.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`)}:${v}`
+		).join(';');
+}
+
+export default themeToCSS;
