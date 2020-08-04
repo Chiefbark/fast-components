c442f2df1cac890879f4ffd603f16dc00ef1ba5a
Jorge Chércoles Moreno <jchercoles@alumnos.imf.com>
2020-08-04 13:56:11 +0200

    added default theme for buttonIcon component

---
+++ src/ButtonIcon/theme.js
@@ -0,0 +1,42 @@
+// Default theme for ButtonIcon Component
+const theme = {
+	root: {
+		default: {
+			display: 'flex',
+			flexDirection: 'row',
+			alignItems: 'center',
+			justifyContent: 'center',
+			backgroundColor: '#2196f3',
+			outline: 'none',
+			borderStyle: 'solid',
+			borderRadius: '4px',
+			borderColor: '#2196f3',
+			color: '#fff',
+			fontSize: '1.15rem',
+			textTransform: 'uppercase',
+			padding: '16px 32px',
+			transition: 'background .25s'
+		},
+		':focus': {
+			backgroundColor: '#1b7dcb'
+		},
+		':hover': {
+			backgroundColor: '#1b7dcb'
+		},
+		':active': {
+			backgroundColor: '#259fff'
+		},
+		'[disabled]': {
+			backgroundColor: '#ffffff1f',
+			color: '#ffffff4d'
+		}
+	},
+	icon: {
+		default: {
+			width: '24px',
+			height: '24px'
+		}
+	}
+}
+
+export default theme;
