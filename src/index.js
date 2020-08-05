60b32c39a25e253ee62da4c54c264605284fa629
Jorge Chércoles Moreno <jchercoles@alumnos.imf.com>
2020-08-05 20:49:07 +0200

    exported textInput

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import _Button from './Button';
 import _ButtonIcon from './ButtonIcon';
 import _Checkbox from './Checkbox';
+import _TextInput from './TextInput';
 
 export const Button = _Button;
 export const ButtonIcon = _ButtonIcon;
 export const Checkbox = _Checkbox;
+export const TextInput = _TextInput;
