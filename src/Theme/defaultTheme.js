7fc081287b8bdb4cacc757af16e5ed01683c8731
Jorge Chércoles Moreno <jchercoles@alumnos.imf.com>
2020-08-09 17:48:17 +0200

    added default theme

---
+++ src/Theme/defaultTheme.js
@@ -0,0 +1,54 @@
+const defaultTheme = {
+	black: {
+		main: '#000',
+		text: '#ddd',
+		'75': '#000000BF',
+		'50': '#00000080',
+		'25': '#00000040'
+	},
+	white: {
+		main: '#fff',
+		text: '#111',
+		'75': '#ffffffBF',
+		'50': '#ffffff80',
+		'25': '#ffffff40'
+	},
+	primary: {
+		light: '#4dabf5',
+		main: '#2196f3',
+		dark: '#1769aa',
+		text: '#ddd'
+	},
+	secondary: {
+		light: '#f73378',
+		main: '#f50057',
+		dark: '#ab003c',
+		text: '#ddd'
+	},
+	error: {
+		light: '#e57373',
+		main: '#f44336',
+		dark: '#d32f2f',
+		text: '#ddd'
+	},
+	warning: {
+		light: '#ffb74d',
+		main: '#ff9800',
+		dark: '#f57c00',
+		text: '#ddd'
+	},
+	info: {
+		light: '#64b5f6',
+		main: '#2196f3',
+		dark: '#1976d2',
+		text: '#ddd'
+	},
+	success: {
+		light: '#81c784',
+		main: '#4caf50',
+		dark: '#388e3c',
+		text: '#ddd'
+	}
+}
+
+export default defaultTheme;
