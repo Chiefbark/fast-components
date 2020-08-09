ebb333b5a5f3fbd1a0ce4f87d834ccf473b0f43c
Jorge Chércoles Moreno <jchercoles@alumnos.imf.com>
2020-08-09 17:47:54 +0200

    updated tests

--- src/TextInput/index.test.js
+++ src/TextInput/index.test.js
@@ -8,31 +8,27 @@ configure({adapter: new Adapter()});
 
 test('TextInput - render', () => {
 	// Default render
-	const wrapper1 = mount(<TextInput/>);
+	const wrapper1 = mount(<TextInput>Label</TextInput>);
 	expect(wrapper1.find('button')).toHaveLength(0);
-	expect(wrapper1.find('label')).toHaveLength(0);
+	expect(wrapper1.find('label')).toHaveLength(1);
+	expect(wrapper1.find('label').text()).toEqual('Label');
 	expect(wrapper1.find('img')).toHaveLength(0);
 
-	// Label render
-	const wrapper2 = mount(<TextInput label={'Label'}/>);
-	expect(wrapper2.find('label')).toHaveLength(1);
-	expect(wrapper2.find('label').text()).toEqual('Label');
-
 	// HelperText render
-	const wrapper3 = mount(<TextInput label={'Label'} helperText={'This is a helper text'}/>);
-	expect(wrapper3.find('small')).toHaveLength(1);
+	const wrapper2 = mount(<TextInput helperText={'This is a helper text'}>Label</TextInput>);
+	expect(wrapper2.find('small')).toHaveLength(1);
 
 	// Error render
-	const wrapper4 = mount(<TextInput label={'Label'} helperText={'This is a helper text'} error={true}/>);
-	expect(wrapper4.find('small').hasClass('error')).toBe(true);
-	expect(wrapper4.find('label').hasClass('error')).toBe(true);
-	expect(wrapper4.find('input').prop('data-error')).toBe(true);
+	const wrapper3 = mount(<TextInput helperText={'This is a helper text'} error={true}>Label</TextInput>);
+	expect(wrapper3.find('small').hasClass('error')).toBe(true);
+	expect(wrapper3.find('label').hasClass('error')).toBe(true);
+	expect(wrapper3.find('input').prop('data-error')).toBe(true);
 
 	// Initial value render
-	const wrapper5 = mount(<TextInput initialValue={'Hello World'}/>);
-	expect(wrapper5.find('input').prop('value')).toEqual('Hello World');
+	const wrapper4 = mount(<TextInput initialValue={'Hello World'}/>);
+	expect(wrapper4.find('input').prop('value')).toEqual('Hello World');
 
 	// Disabled render
-	const wrapper6 = mount(<TextInput disabled={true}/>);
-	expect(wrapper6.find('input').prop('disabled')).toBe(true);
+	const wrapper5 = mount(<TextInput disabled={true}/>);
+	expect(wrapper5.find('input').prop('disabled')).toBe(true);
 });
