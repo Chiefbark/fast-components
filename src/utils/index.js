039e178ad2c5c5ce5abe6f9935905819d092e0b2
Jorge Chércoles Moreno <jchercoles@alumnos.imf.com>
2020-08-15 17:04:43 +0200

    added functions withCSS, getClassName

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,10 @@
-export function stylesToCSS(obj) {
+import hash from 'object-hash';
+import {defaultTheme} from '../Theme';
+
+function stylesToCSS(obj, prefix = '') {
 	return Object.keys(obj).map(x =>
 		Object.keys(obj[x]).map(y =>
-			y === 'default' ? `.${x}{${getStyles(obj[x][y])}}` : `.${x}${y}{${getStyles(obj[x][y])}}`
+			y === 'default' ? `${prefix}.${x}{${getStyles(obj[x][y])}}` : `${prefix}.${x}${y}{${getStyles(obj[x][y])}}`
 		).join('')
 	).join('');
 }
@@ -11,4 +14,33 @@ function getStyles(obj) {
 		.map(([k, v]) =>
 			`${k.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`)}:${v}`
 		).join(';');
+}
+
+export function withCSS(styles, Component, dataId) {
+	if (!document.querySelector(`style[data-id=${dataId}]`)) {
+		const stylesheet = document.createElement('style');
+		stylesheet.setAttribute('type', 'text/css');
+		stylesheet.setAttribute('data-id', dataId);
+		stylesheet.innerText = stylesToCSS(styles, `.${dataId}`);
+		document.head.appendChild(stylesheet);
+	}
+	return Component;
+}
+
+export function getClassName(theme, styles, Component) {
+	const themeHash = hash(theme);
+	const hashIndex = Component.themeHashes.indexOf(themeHash);
+	if (hashIndex < 0) Component.themeHashes.push(themeHash);
+
+	let className = '';
+	if (theme !== defaultTheme) {
+		if (hashIndex < 0) Component.themeID++;
+		className += `-t${hashIndex < 0 ? Component.themeID : hashIndex}`;
+	}
+	if (styles) {
+		Component.stylesID++;
+		className += `-s${Component.stylesID}`;
+	}
+
+	return className;
 }
