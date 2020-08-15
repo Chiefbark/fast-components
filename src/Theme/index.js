4922d5257d09fdce0d21530b9afe761440953cb6
Jorge Chércoles Moreno <jchercoles@alumnos.imf.com>
2020-08-15 17:04:12 +0200

    added defaultTheme

--- src/Theme/index.js
+++ src/Theme/index.js
@@ -1,7 +1,74 @@
 import React from 'react';
-import defaultTheme from './defaultTheme';
 
-const Theme = React.createContext(defaultTheme);
+const _defaultTheme = {
+	black: {
+		main: '#000',
+		'75': '#000000BF',
+		'50': '#00000080',
+		'25': '#00000040'
+	},
+	white: {
+		main: '#fff',
+		'75': '#ffffffbf',
+		'50': '#ffffff80',
+		'25': '#ffffff40'
+	},
+	default: {
+		light: '#f0f0f0',
+		main: '#e0e0e0',
+		dark: '#d0d0d0',
+		text: '#111'
+	},
+	primary: {
+		light: '#4dabf5',
+		main: '#2196f3',
+		dark: '#1769aa',
+		text: '#ddd'
+	},
+	secondary: {
+		light: '#f73378',
+		main: '#f50057',
+		dark: '#ab003c',
+		text: '#ddd'
+	},
+	disabled: {
+		light: '#ffffff1e',
+		main: '#ffffff4c',
+		dark: '#ffffff6b'
+	},
+	muted: {
+		light: '#ffffff',
+		main: '#bfbfbf',
+		dark: '#808080'
+	},
+	error: {
+		light: '#e57373',
+		main: '#f44336',
+		dark: '#d32f2f',
+		text: '#ddd'
+	},
+	warning: {
+		light: '#ffb74d',
+		main: '#ff9800',
+		dark: '#f57c00',
+		text: '#ddd'
+	},
+	info: {
+		light: '#64b5f6',
+		main: '#2196f3',
+		dark: '#1976d2',
+		text: '#ddd'
+	},
+	success: {
+		light: '#81c784',
+		main: '#4caf50',
+		dark: '#388e3c',
+		text: '#ddd'
+	}
+}
+
+const Theme = React.createContext(_defaultTheme);
 
 export const ThemeProvider = Theme.Provider;
 export const ThemeConsumer = Theme.Consumer;
+export const defaultTheme = _defaultTheme;
